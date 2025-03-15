d74e180b43b1eb316c64547ca5d5d74763c1d7b5
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-15 12:03:08 -0500

    feat: View to use on drawer WHEN need formTypes

---
+++ views/ViewFormType/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import FormType from "@/components/Forms/FormTypes";
+import { Box } from "@mui/material";
+
+function ViewFormType() {
+  return (
+    <>
+      <Box sx={{}}>
+        <FormType></FormType>
+      </Box>
+    </>
+  );
+}
+
+export default ViewFormType;
