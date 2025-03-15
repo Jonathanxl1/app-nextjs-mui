83569f85ad0c53022ecf8f123f43a11d7184ab5c
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-15 12:05:34 -0500

    feat: View to use WHEN required Form Properties

---
+++ views/ViewFormProperties/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import FormProperties from "@/components/Forms/FormProperties";
+import { Box } from "@mui/material";
+
+function ViewFormProperties() {
+  return (
+    <>
+      <Box sx={{}}>
+        <FormProperties></FormProperties>
+      </Box>
+    </>
+  );
+}
+
+export default ViewFormProperties;
