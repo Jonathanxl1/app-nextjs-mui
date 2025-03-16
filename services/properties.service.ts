e529e6d00ad39c7c4f6ec9d5ac857ac7dc7ee04a
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-15 22:47:25 -0500

    feat: Added properties services CRUD

---
+++ services/properties.service.ts
@@ -0,0 +1,21 @@
+import api from "./api";
+
+export async function getProperties() {
+  const { data } = await api.get("/properties");
+  return data;
+}
+
+export async function createProperties(payload: unknown) {
+  const { data } = await api.post("/properties", payload);
+  return data;
+}
+
+export async function updateProperties(id: number, payload: unknown) {
+  const { data } = await api.put(`/properties/${id}`, payload);
+  return data;
+}
+
+export async function deleteProperties(id: number) {
+  const { data } = await api.delete(`/properties/${id}`);
+  return data;
+}
