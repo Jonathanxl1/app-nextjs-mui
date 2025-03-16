cdd9ffc139b344cf55f943d13916271ef2f63562
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-16 13:06:19 -0500

    test: Added delay time to mock pending response application

--- mocks/mock.ts
+++ mocks/mock.ts
@@ -1,4 +1,4 @@
 import AxiosMockAdapter from "axios-mock-adapter";
 import api from "@/services/api";
 
-export const mock = new AxiosMockAdapter(api);
+export const mock = new AxiosMockAdapter(api, { delayResponse: 1500 });
