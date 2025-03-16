bd0be26fdac9d947cdf5a066fc64ef621d0f7e00
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-16 13:04:40 -0500

    feat(application): Add state lodging to application - Create external function manipualte state lodging

--- store/application.store.ts
+++ store/application.store.ts
@@ -1,11 +1,20 @@
 import { create } from "zustand";
 
 interface ApplicationState {
+  lodging: boolean;
   showDrawer: boolean;
   toggleDrawer: () => void;
 }
 
 export const useStoreApp = create<ApplicationState>((set) => ({
+  lodging: false,
+  setLodging: (value: boolean) => {
+    set(() => ({ lodging: value }));
+  },
   showDrawer: false,
   toggleDrawer: () => set((state) => ({ showDrawer: !state.showDrawer })),
 }));
+
+export function setLodging(value: boolean) {
+  useStoreApp.setState({ lodging: value });
+}
