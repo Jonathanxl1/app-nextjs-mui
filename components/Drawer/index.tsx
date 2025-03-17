61aea72eb143f5fbb93f0f095abe534348e99f3e
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-17 15:14:07 -0500

    feat(Drawer): Add View Form login

--- components/Drawer/index.tsx
+++ components/Drawer/index.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { Drawer } from "@mui/material";
+
 import ViewFormProperties from "@/views/ViewFormProperties";
 import ViewFormType from "@/views/ViewFormType";
 import { useStoreApp } from "@/store/application.store";
 import { ViewOptions } from "@/interfaces/entity.interface";
 import ViewConfirmationAction from "@/views/ViewConfirmationAction";
+import ViewFormLogin from "@/views/ViewFormLogin";
 
 function DrawerRight() {
   const { showDrawer, closeView, view } = useStoreApp((state) => state);
@@ -18,6 +20,8 @@ function DrawerRight() {
         return <ViewFormType />;
       case "viewConfirmation":
         return <ViewConfirmationAction />;
+      case "viewFormLogin":
+        return <ViewFormLogin />;
       default:
         return null;
     }
