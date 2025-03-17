6eb40eacfab93e1fadf8b4fe6a90ac84bd29432b
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-16 19:14:03 -0500

    feat(Drawer): Add components form to Types and properties - Include dynamic component to set views

--- components/Drawer/index.tsx
+++ components/Drawer/index.tsx
@@ -1,8 +1,29 @@
-import { Drawer } from "@mui/material";
-import { PropsWithChildren } from "react";
+"use client";
 
-function DrawerLeft({ children }: PropsWithChildren) {
-  return <Drawer anchor="right">{children}</Drawer>;
+import { Drawer } from "@mui/material";
+import ViewFormProperties from "@/views/ViewFormProperties";
+import ViewFormType from "@/views/ViewFormType";
+import { useStoreApp, ViewOptions } from "@/store/application.store";
+
+function DrawerRight() {
+  const { showDrawer, closeView, view } = useStoreApp((state) => state);
+
+  function DynamicComponent(type: ViewOptions) {
+    switch (type) {
+      case "viewFormProperties":
+        return <ViewFormProperties />;
+      case "viewFormType":
+        return <ViewFormType />;
+      default:
+        return null;
+    }
+  }
+
+  return (
+    <Drawer open={showDrawer} anchor="right" onClose={closeView}>
+      {DynamicComponent(view)}
+    </Drawer>
+  );
 }
 
-export default DrawerLeft;
+export default DrawerRight;
