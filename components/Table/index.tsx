24562bea7575de5f975961cb940d47409184218e
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-17 07:47:05 -0500

    fix: Unique Id - Fix show id table replace into value row

--- components/Table/index.tsx
+++ components/Table/index.tsx
@@ -1,20 +1,17 @@
 import { TableBody, TableContainer, TableHead, Table } from "@mui/material";
+
 import CustomTableRow from "./CustomTableRow";
 import CustomTableCellActions from "./CustomTableCellActions";
 import CustomTableCell from "./CustomTableCell";
 import { EntityOptions } from "@/interfaces/entity.interface";
+import { TypeElement } from "@/interfaces/types.interface";
+import { PropertiesElement } from "@/interfaces/properties.interface";
 
 interface PropsExtends {
   headers: Array<string>;
-  items?: Array<{
-    name: string;
-    description: string;
-    properties: string[];
-    createdAt: string;
-  }>;
-  showCellAction: boolean;
   origin: EntityOptions;
-  typeAction: string[];
+  items?: Array<TypeElement | PropertiesElement>;
+  showCellAction?: boolean;
 }
 
 function TableRaw({
@@ -33,21 +30,26 @@ function TableRaw({
             </CustomTableRow>
           </TableHead>
           <TableBody>
-            {Array.isArray(items) ? (
-              items.map((item) => (
-                <CustomTableRow key={item.name} cellItems={Object.values(item)}>
-                  {showCellAction ? (
-                    <CustomTableCellActions
-                      origin={origin}
-                      updateAction
-                      deleteAction
-                    />
-                  ) : null}
-                </CustomTableRow>
-              ))
-            ) : (
-              <CustomTableRow cellItems={"Empty Data"} />
-            )}
+            {
+              Array.isArray(items)
+                ? items.map(({ id, ...data }, idx) => (
+                    <CustomTableRow
+                      key={idx}
+                      data-id={id}
+                      cellItems={Object.values(data)}
+                    >
+                      {showCellAction ? (
+                        <CustomTableCellActions
+                          origin={origin}
+                          updateAction
+                          deleteAction
+                        />
+                      ) : null}
+                    </CustomTableRow>
+                  ))
+                : null
+              // <CustomTableRow cellItems={"Empty Data"} />
+            }
           </TableBody>
         </Table>
       </TableContainer>
