b7427f79d7ccbf025959d7c3a114eabce8880866
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-17 16:01:19 -0500

    feat(NavLinkLogin):Add logic to show login state user

--- components/Navbar/NavLinkLogin/index.tsx
+++ components/Navbar/NavLinkLogin/index.tsx
@@ -1,20 +1,56 @@
 import { useStoreApp } from "@/store/application.store";
-import { Button, Grid2, Typography } from "@mui/material";
+import { deleteItem } from "@/utils/localStorage.util";
+import { Button, Grid2, Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
 
 function NavLinkLogin() {
-  const {
-    logged = true,
-    userLogged = { name: "Jonathan" },
-    setView,
-  } = useStoreApp();
+  const { logged, user, setView, restoreUser } = useStoreApp();
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Grid2>
       {logged ? (
+        <Grid2>
+          <Button
+            color="inherit"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            ¡Hi {user ? user.name : ""}!
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={() => {
+              handleClose();
+            }}
+          >
+            <MenuItem
+              onClick={() => {
+                restoreUser();
+                deleteItem("token");
+                handleClose();
+              }}
+            >
+              Cerrar Sesion
+            </MenuItem>
+          </Menu>
+        </Grid2>
+      ) : (
         <Button color="inherit" onClick={() => setView("viewFormLogin")}>
           Login
         </Button>
-      ) : (
-        <Typography>Hi!, {userLogged.name}</Typography>
       )}
     </Grid2>
   );
