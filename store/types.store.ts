406c654e504e2ccf695260429e393c8c28d2dc90
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-17 18:01:44 -0500

    feat(Types Store): Add state selected Type to managa with action - Add method to set Type into selected state - Add method to retrieve type

--- store/types.store.ts
+++ store/types.store.ts
@@ -1,15 +1,19 @@
 import { create } from "zustand";
 
-import { getTypes } from "@/services/types.service";
+import { getTypes, updateType } from "@/services/types.service";
 import { setLoading } from "./application.store";
 import { TypeElement } from "@/interfaces/types.interface";
 
 interface TypeStore {
   data: Array<TypeElement>;
   retriveTypes: () => void;
+  selectedType: TypeElement | null;
+  setSelectedType: (id: TypeElement["id"]) => void;
+  updateType: (id: TypeElement["id"], payload: TypeElement) => void;
+  getType: (id: TypeElement["id"]) => TypeElement;
 }
 
-export const useTypeStore = create<TypeStore>((set) => ({
+export const useTypeStore = create<TypeStore>((set, get) => ({
   data: [],
   retriveTypes: () => {
     setLoading(true);
@@ -18,5 +22,19 @@ export const useTypeStore = create<TypeStore>((set) => ({
       .finally(() => {
         setLoading(false);
       });
+  },
+  updateType: (id, payload) => {
+    updateType(id, payload).then(() => {
+      get().retriveTypes();
+    });
+  },
+  getType: (idType) => {
+    const [selectedObjectType] = get().data.filter(({ id }) => idType == id);
+    return selectedObjectType;
+  },
+  selectedType: null,
+  setSelectedType: (id) => {
+    const selectedType = get().getType(id);
+    set((state) => ({ ...state, selectedType }));
   },
 }));
