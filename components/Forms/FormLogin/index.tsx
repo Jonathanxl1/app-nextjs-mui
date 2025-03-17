7670680b3a2a8e9c14a9f697c7d789aae1522bb3
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-17 15:12:15 -0500

    feat(LoginForm): Include service to login - Add state to manage form login state - Include logic to login user by form

--- components/Forms/FormLogin/index.tsx
+++ components/Forms/FormLogin/index.tsx
@@ -1,12 +1,56 @@
+import { login } from "@/services/authentication.service";
+import { useStoreApp } from "@/store/application.store";
+import { registerItem } from "@/utils/localStorage.util";
 import { Button, TextField } from "@mui/material";
+import { ChangeEvent, useState } from "react";
+
+interface Login {
+  email: "";
+  password: "";
+}
 
 function FormLogin() {
+  const [form, setForm] = useState<Login>({
+    email: "",
+    password: "",
+  });
+
+  const { closeView, setUser } = useStoreApp();
+
+  function inputForm(
+    key: string,
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const value = e.target.value;
+
+    setForm((state) => ({ ...state, [key]: value }));
+  }
+
+  function submitForm() {
+    login(form).then((data) => {
+      if (data) {
+        const { accessToken, user } = data;
+        registerItem("token", accessToken);
+        setUser(user);
+        closeView();
+      }
+    });
+  }
+
   return (
     <>
-      <form>
-        <TextField label="Email" type="text" />
-        <TextField label="Password" type="password" />
-        <Button>Login</Button>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <TextField
+          label="Email"
+          type="text"
+          onChange={(e) => inputForm("email", e)}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          onChange={(e) => inputForm("password", e)}
+        />
+        <Button onClick={submitForm}>Login</Button>
       </form>
     </>
   );
