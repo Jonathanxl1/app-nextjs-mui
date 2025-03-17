ef5a612acfdb03c655455884217b31f02c516f2c
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-16 19:10:32 -0500

    feat(Table):Add prop origin by action - Include methods setAction  and setView to help set view on Drawer - Include method to set view depends of the context

--- components/Table/CustomTableCellActions/index.tsx
+++ components/Table/CustomTableCellActions/index.tsx
@@ -1,22 +1,48 @@
+"use client";
 import { Grid2, IconButton, TableCell } from "@mui/material";
 
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import {
+  RolePermissions,
+  useStoreApp,
+  ViewOptions,
+} from "@/store/application.store";
+import { EntityOptions } from "@/interfaces/entity.interface";
 
 interface PropsTableCellAction {
   updateAction: boolean;
   deleteAction: boolean;
+  origin: EntityOptions;
 }
+
+type ObjectEntity = {
+  types: ViewOptions;
+  properties: ViewOptions;
+};
+
+const objectEntity: ObjectEntity = {
+  types: "viewFormType",
+  properties: "viewFormProperties",
+};
 
 function CustomTableCellActions({
   updateAction,
   deleteAction,
+  origin = "types",
 }: Partial<PropsTableCellAction>) {
+  const { setView, setAction } = useStoreApp((state) => state);
+
+  function openView(value: ViewOptions, action: RolePermissions) {
+    setView(value);
+    setAction(action);
+  }
+
   return (
     <TableCell>
       <Grid2 container>
         {updateAction && (
-          <IconButton>
+          <IconButton onClick={() => openView(objectEntity[origin], "update")}>
             <EditIcon />
           </IconButton>
         )}
