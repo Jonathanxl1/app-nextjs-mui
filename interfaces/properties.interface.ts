8fa6bfaebbec226d7315de907c00d57384959eb7
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-16 23:00:32 -0500

    feat: Add interface Properties and Type property input

---
+++ interfaces/properties.interface.ts
@@ -0,0 +1,8 @@
+export type typePropertyInput = "text" | "number" | "date" | "check";
+
+export interface PropertiesElement {
+  id: number;
+  name: string;
+  type: typePropertyInput;
+  createAt: Date;
+}
