25608164525b294a07a33bf5d1fa5d2b873a5456
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-16 23:01:39 -0500

    feat: Add defaultPropertiesInput - Moved interface to folder interfaces

--- store/properties.store.ts
+++ store/properties.store.ts
@@ -1,19 +1,20 @@
-import { getProperties } from "@/services/properties.service";
 import { create } from "zustand";
 
-interface PropertiesElement {
-  name: string;
-  type: string;
-  createAt: Date;
-}
+import {
+  PropertiesElement,
+  typePropertyInput,
+} from "@/interfaces/properties.interface";
+import { getProperties } from "@/services/properties.service";
 
 interface PropertiesStore {
   data: Array<PropertiesElement>;
   retrieveProperties: () => void;
+  defaultPropertiesInput: typePropertyInput[];
 }
 
 export const usePropertiesStore = create<PropertiesStore>((set) => ({
   data: [],
+  defaultPropertiesInput: ["date", "check", "number", "text"],
   retrieveProperties: () => {
     getProperties().then((items) => set(() => ({ data: items })));
   },
