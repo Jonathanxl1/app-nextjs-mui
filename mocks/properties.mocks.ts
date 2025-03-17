82cf066a53366fd8276814a5593486c866953da0
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-17 09:02:37 -0500

    chore: Update mock properties

--- mocks/properties.mocks.ts
+++ mocks/properties.mocks.ts
@@ -3,18 +3,27 @@ import { mock } from "./mock";
 function MockPropertiesGet() {
   mock.onGet("/properties").reply(200, [
     {
+      id: 1,
       name: "nombre",
       type: "text",
       date: "2025-04-03",
     },
     {
+      id: 2,
       name: "Fecha de Nacimiento",
       type: "date",
       date: "2025-04-03",
     },
     {
+      id: 3,
       name: "Telefono",
       type: "number",
+      date: "2025-04-03",
+    },
+    {
+      id: 4,
+      name: "Termino y Condiciones",
+      type: "check",
       date: "2025-04-03",
     },
   ]);
