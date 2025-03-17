58c9b50a38e992748b89f2282d9e360bad9b551a
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-17 17:59:28 -0500

    feat(Properties Store): Add state selected property to use on action - Add method to retrieve property - Add method to set property into selected state - Add method to update properties

--- store/properties.store.ts
+++ store/properties.store.ts
@@ -4,18 +4,45 @@ import {
   PropertiesElement,
   typePropertyInput,
 } from "@/interfaces/properties.interface";
-import { getProperties } from "@/services/properties.service";
+import { getProperties, updateProperties } from "@/services/properties.service";
 
 interface PropertiesStore {
   data: Array<PropertiesElement>;
   retrieveProperties: () => void;
   defaultPropertiesInput: typePropertyInput[];
+  selectedProperty: PropertiesElement | null;
+  setSelectedProperty: (id: PropertiesElement["id"]) => void;
+  updateProperty: (
+    value: PropertiesElement["id"],
+    payload: PropertiesElement
+  ) => void;
+  getProperty: (id: PropertiesElement["id"]) => PropertiesElement;
 }
 
-export const usePropertiesStore = create<PropertiesStore>((set) => ({
+export const usePropertiesStore = create<PropertiesStore>((set, get) => ({
   data: [],
   defaultPropertiesInput: ["date", "check", "number", "text"],
   retrieveProperties: () => {
     getProperties().then((items) => set(() => ({ data: items })));
+  },
+
+  updateProperty: (
+    idProp: PropertiesElement["id"],
+    payload: PropertiesElement
+  ) => {
+    // Mock update property
+    updateProperties(idProp, payload).then(() => {
+      get().retrieveProperties();
+    });
+  },
+  getProperty: (idProp: PropertiesElement["id"]) => {
+    const [propertyObject] = get().data.filter(({ id }) => idProp == id);
+    return propertyObject;
+  },
+
+  selectedProperty: null,
+  setSelectedProperty: (id: PropertiesElement["id"]) => {
+    const selectedProperty = get().getProperty(id);
+    set((state) => ({ ...state, selectedProperty }));
   },
 }));
