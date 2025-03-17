4c81534a13e7a3aaec18a5647b82a41c527d0ca2
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-17 18:15:35 -0500

    feat(Properties Service): Add type to paramenters functions

--- services/properties.service.ts
+++ services/properties.service.ts
@@ -1,24 +1,28 @@
 import { MockPropierties } from "@/mocks/properties.mocks";
 import api from "./api";
+import { PropertiesElement } from "@/interfaces/properties.interface";
 
 MockPropierties();
 
 export async function getProperties() {
-  const { data } = await api.get("/properties");
+  const { data } = await api.get<PropertiesElement>("/properties");
   return data;
 }
 
-export async function createProperties(payload: unknown) {
+export async function createProperties(payload: PropertiesElement) {
   const { data } = await api.post("/properties", payload);
   return data;
 }
 
-export async function updateProperties(id: number, payload: unknown) {
+export async function updateProperties(
+  id: PropertiesElement["id"],
+  payload: PropertiesElement
+) {
   const { data } = await api.put(`/properties/${id}`, payload);
   return data;
 }
 
-export async function deleteProperties(id: number) {
+export async function deleteProperties(id: PropertiesElement["id"]) {
   const { data } = await api.delete(`/properties/${id}`);
   return data;
 }
