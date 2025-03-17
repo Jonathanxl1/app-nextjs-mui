98ba940a5197060e2fce67c12e0cf155f7da1338
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-17 09:13:32 -0500

    feat: Add view login form on Drawer

---
+++ views/ViewFormLogin/index.tsx
@@ -0,0 +1,14 @@
+import FormLogin from "@/components/Forms/FormLogin";
+import { Box } from "@mui/material";
+
+function ViewFormLogin() {
+  return (
+    <>
+      <Box sx={{}}>
+        <FormLogin></FormLogin>
+      </Box>
+    </>
+  );
+}
+
+export default ViewFormLogin;
