d4ef22e749fa64a7e18c5a4c8c0aac638dc3f19b
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-16 23:10:01 -0500

    feat(form Type):  Add useState manage form - Set behabior multiple Select and checkbox

--- components/Forms/FormTypes/index.tsx
+++ components/Forms/FormTypes/index.tsx
@@ -1,23 +1,76 @@
 "use client";
 
-import { useStoreApp } from "@/store/application.store";
+import { ChangeEvent, useState } from "react";
 import {
   Box,
   Button,
+  Checkbox,
+  FormControl,
   FormGroup,
   InputLabel,
+  ListItemText,
+  MenuItem,
   Select,
+  SelectChangeEvent,
   TextareaAutosize,
   TextField,
   Typography,
 } from "@mui/material";
+
+import { useStoreApp } from "@/store/application.store";
+import { usePropertiesStore } from "@/store/properties.store";
+
+interface PropertyType {
+  id: number;
+  name: string;
+}
+interface FormType {
+  name: string;
+  description: string;
+  properties: PropertyType["id"][];
+}
 
 type PropsFormsTypes = {
   submitFunction?: () => void;
 };
 
 function FormType({ submitFunction = () => {} }: PropsFormsTypes) {
+  const { data: dataProperties } = usePropertiesStore((state) => state);
   const { action } = useStoreApp((state) => state);
+
+  const [form, setForm] = useState<FormType>({
+    name: "",
+    description: "",
+    properties: [],
+  });
+
+  function stringPropertiesSelected(selected: number[], data: PropertyType[]) {
+    return data
+      .filter(({ id }) => selected.includes(id))
+      .map(({ name }) => name)
+      .join(", ");
+  }
+
+  function setInput(
+    key: string,
+    value:
+      | ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+      | SelectChangeEvent<typeof form.properties>
+  ) {
+    const inputValue: string | number[] = value.target.value;
+
+    if (typeof inputValue !== "string") {
+      setForm((state) => ({
+        ...state,
+        [key]: inputValue,
+      }));
+    } else {
+      setForm((state) => ({
+        ...state,
+        [key]: inputValue,
+      }));
+    }
+  }
 
   return (
     <>
@@ -33,12 +86,35 @@ function FormType({ submitFunction = () => {} }: PropsFormsTypes) {
           {action == "create" ? "Crear Nuevo Tipos" : "Actualizar Tipo"}
         </Typography>
         <form style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
-          <TextField variant="outlined" label="Nombre" />
+          <TextField
+            variant="outlined"
+            label="Nombre"
+            onChange={(e) => setInput("name", e)}
+          />
           <FormGroup>
             <InputLabel>Description </InputLabel>
-            <TextareaAutosize />
+            <TextareaAutosize onChange={(e) => setInput("description", e)} />
           </FormGroup>
-          <Select label="Selecciona el tipo"></Select>
+          <FormControl>
+            <InputLabel id="property-label">Properties</InputLabel>
+            <Select
+              label="Properties"
+              labelId="property-label"
+              multiple
+              onChange={(e) => setInput("properties", e)}
+              value={form.properties}
+              renderValue={(selected) =>
+                stringPropertiesSelected(selected, dataProperties)
+              }
+            >
+              {dataProperties.map(({ id, name }) => (
+                <MenuItem key={id} value={id}>
+                  <Checkbox checked={form.properties.includes(id)} />
+                  <ListItemText primary={name} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
           <Button variant="contained" onClick={submitFunction}>
             {action == "create" ? "Crear" : "Actualizar"}
           </Button>
