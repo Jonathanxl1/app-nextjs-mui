e5bbd3cdd73b86f4f428a2d924c6143b9cad00c8
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-18 08:44:18 -0500

    feat(View Confirmation Action):Add methods to confirm action or cancel action

--- views/ViewConfirmationAction/index.tsx
+++ views/ViewConfirmationAction/index.tsx
@@ -1,14 +1,25 @@
+import { useStoreApp } from "@/store/application.store";
 import { Button, Grid2, Typography } from "@mui/material";
 
 function ViewConfirmationAction() {
+  const { closeView, callConfirmMethod } = useStoreApp((state) => state);
+
   return (
     <Grid2>
       <Grid2>
         <Typography>Seguro desea eliminar este Elemento</Typography>
       </Grid2>
       <Grid2>
-        <Button>Confirmar</Button>
-        <Button>Cancelar</Button>
+        <Button
+          onClick={() => {
+            callConfirmMethod().then(() => {
+              closeView();
+            });
+          }}
+        >
+          Confirmar
+        </Button>
+        <Button onClick={closeView}>Cancelar</Button>
       </Grid2>
     </Grid2>
   );
