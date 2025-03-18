71f6094fbcc8b06ca3d56172a1e10b8d3959699a
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-18 11:51:18 -0500

    feat(Home Page): Add Page to show Table properties and typos - Add logic to show table WHEN user its logged - Add logic to load properties when load page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,91 @@
+"use client";
+
+import HeaderTable from "@/components/HeaderTable";
+import SearchBar from "@/components/SearchBar";
+import TableRaw from "@/components/Table";
+import { useStoreApp } from "@/store/application.store";
+import { usePropertiesStore } from "@/store/properties.store";
+import { useTypeStore } from "@/store/types.store";
+import { Box, Grid2, Typography } from "@mui/material";
+import { useEffect } from "react";
+
+const headersTypes = [
+  "Nombre",
+  "Description",
+  "Propiedades",
+  "Fecha de Creacion",
+];
+
+const headersProperties = ["Nombre", "Tipo de Propiedad", "Fecha de Creacion"];
+
 export default function Home() {
+  const { hasPermission, isSearching } = useStoreApp();
+  const {
+    data: dataTypes,
+    retriveTypes,
+    dataFiltered: filteredTypes,
+  } = useTypeStore((state) => state);
+  const {
+    data: dataProperties,
+    retrieveProperties,
+    dataFiltered: filteredProperties,
+  } = usePropertiesStore((state) => state);
+
+  useEffect(() => {
+    retriveTypes();
+    retrieveProperties();
+    return () => {};
+  }, []);
+
   return (
-    <div className="">
-      <h2>Hello world</h2>
-    </div>
+    <>
+      {hasPermission("read") ? (
+        <>
+          <SearchBar />
+          <Box sx={{ m: 4 }}>
+            <HeaderTable
+              origin="types"
+              title="Tipos"
+              variant="h4"
+              createAction={hasPermission("create")}
+            />
+            <TableRaw
+              headers={headersTypes}
+              items={isSearching ? filteredTypes : dataTypes}
+              origin="types"
+              updateAction={hasPermission("update")}
+              deleteAction={hasPermission("delete")}
+            />
+          </Box>
+
+          <Box sx={{ m: 4 }}>
+            <HeaderTable
+              createAction={hasPermission("create")}
+              origin="properties"
+              title="Propiedades"
+              variant="h4"
+            />
+            <TableRaw
+              headers={headersProperties}
+              items={isSearching ? filteredProperties : dataProperties}
+              origin="properties"
+              updateAction={hasPermission("update")}
+              deleteAction={hasPermission("delete")}
+            />
+          </Box>
+        </>
+      ) : (
+        <Grid2
+          alignItems="center"
+          justifyContent="center"
+          sx={{ minWidth: "100vw", height: "100svh", mx: "auto", my: 0 }}
+        >
+          <Typography variant="h1">Hola Bienvenidos Fortex Design</Typography>
+          <Typography variant="caption">
+            Para ver lo tipos y propiedades disponible Inicia Sesion
+          </Typography>
+        </Grid2>
+      )}
+    </>
   );
 }
