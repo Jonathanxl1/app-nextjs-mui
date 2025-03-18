a4017f76e884bb41f2e09e66c3af9d119883c4ca
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-17 19:58:47 -0500

    feat(Type Mock): Add mock to create type and update Array Elements - Move data mock to instance value to use on mocks

--- mocks/types.mock.ts
+++ mocks/types.mock.ts
@@ -3,34 +3,43 @@ import { mock } from "./mock";
 const typeUri = "/types";
 const url = new RegExp(`${typeUri}/*`);
 
+const storeMockProperties = [
+  {
+    id: 1,
+    name: "Persona",
+    description: "Categoria para personas",
+    properties: [],
+    createdAt: new Date(Date.now()).toDateString(),
+  },
+  {
+    id: 2,
+    name: "Organizacion",
+    description: "Categoria para Organizaciones",
+    properties: [],
+    createdAt: new Date(Date.now()).toDateString(),
+  },
+  {
+    id: 3,
+    name: "Evento",
+    description: "Categoria para Eventos",
+    properties: [],
+    createdAt: new Date(Date.now()).toDateString(),
+  },
+];
+
 export async function MockGetTypes() {
-  mock.onGet("/types").reply(200, [
-    {
-      id: 1,
-      name: "Persona",
-      description: "Categoria para personas",
-      properties: [],
-      createdAt: new Date(Date.now()).toDateString(),
-    },
-    {
-      id: 2,
-      name: "Organizacion",
-      description: "Categoria para Organizaciones",
-      properties: [],
-      createdAt: new Date(Date.now()).toDateString(),
-    },
-    {
-      id: 3,
-      name: "Evento",
-      description: "Categoria para Eventos",
-      properties: [],
-      createdAt: new Date(Date.now()).toDateString(),
-    },
-  ]);
+  mock.onGet("/types").reply(200, storeMockProperties);
 }
 
 export async function MockCreateType() {
-  mock.onPost("/types").reply(204, mock.history.post);
+  mock.onPost("/types").reply((config) => {
+    const { data } = config;
+    const objectType = JSON.parse(data);
+    objectType.id = storeMockProperties.length + 1;
+    objectType.createdAt = new Date(Date.now()).toDateString();
+    storeMockProperties.push(objectType);
+    return [204, {}];
+  });
 }
 
 export async function MockUpdateType() {
