14bfe294f8280e21870391201a55f0825382e777
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-18 08:45:44 -0500

    feat(Properties Mock): Update mocks to behavior backend server

--- mocks/properties.mocks.ts
+++ mocks/properties.mocks.ts
@@ -1,36 +1,79 @@
 import { mock } from "./mock";
 
+const storeMockProperties = [
+  {
+    id: 1,
+    name: "nombre",
+    type: "text",
+    date: new Date(Date.now()).toDateString(),
+  },
+  {
+    id: 2,
+    name: "Fecha de Nacimiento",
+    type: "date",
+    date: new Date(Date.now()).toDateString(),
+  },
+  {
+    id: 3,
+    name: "Telefono",
+    type: "number",
+    date: new Date(Date.now()).toDateString(),
+  },
+  {
+    id: 4,
+    name: "Termino y Condiciones",
+    type: "check",
+    date: new Date(Date.now()).toDateString(),
+  },
+];
+
 function MockPropertiesGet() {
-  mock.onGet("/properties").reply(200, [
-    {
-      id: 1,
-      name: "nombre",
-      type: "text",
-      date: "2025-04-03",
-    },
-    {
-      id: 2,
-      name: "Fecha de Nacimiento",
-      type: "date",
-      date: "2025-04-03",
-    },
-    {
-      id: 3,
-      name: "Telefono",
-      type: "number",
-      date: "2025-04-03",
-    },
-    {
-      id: 4,
-      name: "Termino y Condiciones",
-      type: "check",
-      date: "2025-04-03",
-    },
-  ]);
+  mock.onGet("/properties").reply(200, storeMockProperties);
 }
-function MockPropertiesPost() {}
-function MockPropertiesPut() {}
-function MockPropertiesDelete() {}
+function MockPropertiesPost() {
+  mock.onPost("/properties").reply(({ data }) => {
+    const propertyObject = JSON.parse(data);
+    propertyObject.id = storeMockProperties.length + 1;
+    propertyObject.createdAt = new Date(Date.now()).toDateString();
+    storeMockProperties.push(propertyObject);
+    return [201, {}];
+  });
+}
+function MockPropertiesPut() {
+  const propertiesUri = "/properties";
+  const url = new RegExp(`${propertiesUri}/*`);
+  mock.onPut(url).reply(({ data }) => {
+    const propertyObject = JSON.parse(data);
+    const indexProp = storeMockProperties
+      .map(({ id }) => id)
+      .indexOf(propertyObject.id);
+    storeMockProperties[indexProp] = {
+      ...storeMockProperties[indexProp],
+      ...propertyObject,
+    };
+    return [201, {}];
+  });
+}
+function MockPropertiesDelete() {
+  const propertiesUri = "/properties";
+  const url = new RegExp(`${propertiesUri}/*`);
+  const matchIdRegex = new RegExp(/(\d+)/);
+  mock.onDelete(url).reply((config) => {
+    const { url: uri } = config;
+    const match = uri?.match(matchIdRegex);
+    let idMatch = "-1";
+    if (match && match.length) {
+      idMatch = match[1];
+    }
+    const indexProp = storeMockProperties
+      .map(({ id }) => id)
+      .indexOf(parseInt(idMatch));
+    if (indexProp >= 0) {
+      storeMockProperties.splice(indexProp, 1);
+    }
+    return [200, {}];
+  });
+}
 
 export function MockPropierties() {
   MockPropertiesGet();
