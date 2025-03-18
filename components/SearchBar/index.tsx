d8eb9acce226ed6a8ba8db4be782f9de5f3b0de9
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-18 11:43:13 -0500

    feat(Search Bar):Add component search bar to filter items intos table - Add filter to Properties -Add filter to Types

---
+++ components/SearchBar/index.tsx
@@ -0,0 +1,76 @@
+import { Grid2, IconButton, InputAdornment, TextField } from "@mui/material";
+
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
+import { ChangeEvent, useEffect } from "react";
+import { usePropertiesStore } from "@/store/properties.store";
+import { useStoreApp } from "@/store/application.store";
+import { useTypeStore } from "@/store/types.store";
+
+function SearchBar() {
+  const { search, setSearch } = useStoreApp();
+  const { filterProperties } = usePropertiesStore();
+  const { filterTypes } = useTypeStore();
+
+  useEffect(() => {
+    filterProperties(search);
+    filterTypes(search);
+
+    return () => {};
+  }, [search]);
+
+  function searchInput(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    const value = e.target.value;
+    if (value) {
+      setSearch(value);
+    } else {
+      setSearch("");
+    }
+  }
+
+  function cleanInput() {
+    setSearch("");
+  }
+
+  return (
+    <Grid2
+      container
+      size={{ lg: 10 }}
+      sx={{
+        justifyContent: "center",
+        my: "30px",
+      }}
+    >
+      <Grid2 size={{ lg: 6 }}>
+        <TextField
+          label="Search"
+          placeholder="Search"
+          variant="standard"
+          sx={{
+            width: "100%",
+          }}
+          value={search}
+          onChange={searchInput}
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+              endAdornment: search ? (
+                <InputAdornment position="end">
+                  <IconButton onClick={cleanInput}>
+                    <CloseIcon />
+                  </IconButton>
+                </InputAdornment>
+              ) : null,
+            },
+          }}
+        />
+      </Grid2>
+    </Grid2>
+  );
+}
+
+export default SearchBar;
