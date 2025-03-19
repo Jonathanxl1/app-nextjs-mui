b6349801abc0f79660ebff1008a7b4e8a6214a97
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-18 22:32:24 -0500

    feat(Navbar): Added Behavior to resposive design and login session - Include design responsive

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,6 +2,9 @@
 
 import {
   AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
   Toolbar,
   Typography,
   useMediaQuery,
@@ -10,16 +13,74 @@ import {
 
 import MenuIcon from "@mui/icons-material/Menu";
 import NavLinkLogin from "./NavLinkLogin";
+import { useStoreApp } from "@/store/application.store";
+import { useState } from "react";
+
+import { deleteItem } from "@/utils/localStorage.util";
 
 function Navbar() {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down("sm"));
 
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const { logged, user, restoreUser } = useStoreApp();
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography sx={{ flexGrow: 1 }}>Fortex Design</Typography>
-        {matches ? <MenuIcon /> : <NavLinkLogin />}
+        {matches ? (
+          <>
+            <IconButton
+              aria-controls={open ? "menu-options" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              {logged ? (
+                <Typography color="white">
+                  ¡Hi {user ? user.name : ""}!
+                </Typography>
+              ) : (
+                <MenuIcon />
+              )}
+            </IconButton>
+            <Menu
+              id="menu-options"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem
+                onClick={() => {
+                  if (logged) {
+                    restoreUser();
+                    deleteItem("token");
+                  }
+                  handleClose();
+                }}
+              >
+                {logged ? (
+                  <Typography>Cerrar Session</Typography>
+                ) : (
+                  <NavLinkLogin />
+                )}
+              </MenuItem>
+            </Menu>
+          </>
+        ) : (
+          <NavLinkLogin />
+        )}
       </Toolbar>
     </AppBar>
   );
