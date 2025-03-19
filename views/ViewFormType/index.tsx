6a46f1cad20d68db65ea20199d3c145d35264f1d
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-03-18 22:27:08 -0500

    style: Add Style and responsive design Form

--- views/ViewFormType/index.tsx
+++ views/ViewFormType/index.tsx
@@ -5,11 +5,18 @@ import { Box } from "@mui/material";
 
 function ViewFormType() {
   return (
-    <>
-      <Box sx={{}}>
-        <FormType></FormType>
-      </Box>
-    </>
+    <Box
+      sx={{
+        display: "grid",
+        height: "80%",
+        flexDirection: "row",
+        justifyItems: "center",
+        alignItems: "center",
+        backgroundColor: "primary",
+      }}
+    >
+      <FormType></FormType>
+    </Box>
   );
 }
 
